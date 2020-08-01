89f23e2524b1768438e02cbb706cd5b9c03ea4a9
vishesh <kvishesh89@gmail.com>
2020-08-01 21:14:28 +0530

    Minor fixes

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 	Roll_No = models.IntegerField(default='180101089')
-	Department = models.CharField(default='CSE',max_length=100)
+	Department = models.CharField(max_length=100)
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
